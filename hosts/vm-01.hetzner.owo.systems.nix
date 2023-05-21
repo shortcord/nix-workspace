034b2578be26f9811a3dac284e6b4e86803037df
Short Cord <short@shortcord.com>
2023-05-21 07:28:23 -0400

    Working on getting blackbox exporter working

--- hosts/vm-01.hetzner.owo.systems.nix
+++ hosts/vm-01.hetzner.owo.systems.nix
@@ -190,6 +190,32 @@ in { name, nodes, pkgs, lib, config, modulesPath, ... }: {
         scrape_interval = "5s";
       };
       scrapeConfigs = [
+        # {
+        #   job_name = "blackbox-exporters";
+        #   metrics_path = "/probe";
+        #   params = { module = [ "icmp" ]; };
+        #   static_configs = [{
+        #     targets = [
+        #       "home.shortcord.com"
+        #       "router.cloud.shortcord.com"
+        #       "violet.home.shortcord.com"
+        #     ];
+        #   }];
+        #   relabel_configs = [
+        #     {
+        #       source_labels = [ "__address__" ];
+        #       target_label = "__param_target";
+        #     }
+        #     {
+        #       source_labels = [ "__param_target" ];
+        #       target_label = "instance";
+        #     }
+        #     {
+        #       target_label = "__address__";
+        #       replacement = "127.0.0.1:9115";
+        #     }
+        #   ];
+        # }
         {
           job_name = "node-exporters";
           dns_sd_configs = [{
@@ -253,7 +279,7 @@ in { name, nodes, pkgs, lib, config, modulesPath, ... }: {
     #   backend = "docker";
     #   containers. = {
     #     "moustail.dev" = {
-          
+
     #     };
     #   };
     # };
