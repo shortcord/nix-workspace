75466035173e27fe6826bfb428d03cf484c68d9b
Short Cord <short@shortcord.com>
2023-06-01 17:49:56 -0400

    Updated nixos to 23.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/23.05";
     colmena.url = "github:zhaofengli/colmena/v0.3.2";
     ragenix.url = "github:yaxitech/ragenix";
   };
@@ -10,6 +10,7 @@
       sshkeys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINaxLI7oCJcUxfjGXXgs9YI7DimlFbtWE+R22jDF6Zxl short@maus"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEUi5rrB0okX4gQUsivnujVY+0ggin5zKTJMP7ynwKLU short@surface"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPWfoWfo/L6yoIwCbnV7IwfsSFrrrnt6cQpoX60YDaQ0 short@mauspad"
       ];
     in {
       colmena = {
