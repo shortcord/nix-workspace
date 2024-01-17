4b75833c612fdd6563d81e3a2bf5756aa9b21875
Short Cord <short@shortcord.com>
2024-01-17 06:24:27 -0500

    Moved Pterodactyl to websites folder for vm-01

--- hosts/vm-01.hetzner.owo.systems/websites.nix
+++ hosts/vm-01.hetzner.owo.systems/websites.nix
@@ -3,6 +3,7 @@
         ./websites/shortcord.com.nix
         ./websites/owo.solutions.nix
         ./websites/mousetail.dev.nix
+        ./websites/pterodactyl.nix
         ./websites/speedtest.nix
     ];
 }
