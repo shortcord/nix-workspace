1ff8ea796160ebe3e841fc18cdededd2af0a646b
Short Cord <short@shortcord.com>
2024-01-22 09:24:57 -0500

    Cleanup of lilac

--- hosts/lilac.lab.shortcord.com.nix
+++ hosts/lilac.lab.shortcord.com.nix
@@ -24,42 +24,6 @@ in {
     ./${name}/postgresql.nix
   ];
 
-  # systemd.network = {
-  #   enable = true;
-  #   netdevs = {
-  #     "50-wg0" = {
-  #       netdevConfig = {
-  #         Kind = "wireguard";
-  #         Name = "wg0";
-  #         MTUBytes = "1300";
-  #       };
-  #       wireguardConfig = {
-  #         PrivateKeyFile = config.age.secrets.wireguardPrivateKey.path;
-  #         ListenPort = 51820;
-  #       };
-  #       wireguardPeers = [{
-  #         wireguardPeerConfig = {
-  #           PublicKey = "ePYkBTYZaul66VdGLG70IZcCvIaZ7aSeRrkb+hskhiQ=";
-  #           AllowedIPs = [ "10.6.210.1/32" "10.6.210.0/24" "0.0.0.0/0" ];
-  #           Endpoint = "147.135.125.64:51820";
-  #           RouteTable = "off";
-  #         };
-  #       }];
-  #     };
-  #   };
-  #   networks.wg0 = {
-  #     matchConfig.Name = "wg0";
-  #     address = [ "10.6.210.29/32" ];
-  #     routes = [{
-  #       routeConfig = {
-  #         Gateway = "10.6.210.1";
-  #         Destination = "0.0.0.0/0";
-  #         Metric = 0;
-  #       };
-  #     }];
-  #   };
-  # };
-
   networking = {
     useDHCP = true;
     nameservers = [ "10.18.0.1" ];
@@ -118,31 +82,4 @@ in {
       };
     };
   };
-
-  # Ensure that all wireguard tunnels are up
-  # There is probably a better way for this but idk
-  # systemd = {
-  #   timers = {
-  #     "check-wireguard-tunnels" = {
-  #       wantedBy = [ "timers.target" ];
-  #       timerConfig = {
-  #         OnBootSec = "5m";
-  #         OnUnitActiveSec = "5m";
-  #         Unit = "check-wireguard-tunnels.service";
-  #       };
-  #     };
-  #   };
-  #   services = {
-  #     "check-wireguard-tunnels" = {
-  #       script = ''
-  #         ${pkgs.iputils}/bin/ping -qc1 -w1 10.6.210.1 > /dev/null || systemctl restart wireguard-wg0.service
-  #         ${pkgs.iputils}/bin/ping -qc1 -w1 10.7.210.1 > /dev/null || systemctl restart wireguard-mail-relay.service          
-  #       '';
-  #       serviceConfig = {
-  #         Type = "oneshot";
-  #         User = "root";
-  #       };
-  #     };
-  #   };
-  # };
 }
