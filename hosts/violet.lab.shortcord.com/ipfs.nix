fd80160c16a7e121ecca287e537cbc0154c8d0fd
Short Cord <short@shortcord.com>
2024-02-04 01:18:10 -0500

    Cleaning up IPFS and adding addresses

--- hosts/violet.lab.shortcord.com/ipfs.nix
+++ hosts/violet.lab.shortcord.com/ipfs.nix
@@ -40,7 +40,8 @@
       enable = true;
       emptyRepo = true;
       enableGC = true;
-      localDiscovery = false;
+      autoMigrate = false;
+      localDiscovery = true;
       settings = {
         PublicGateways = {
           "${config.networking.fqdn}" = {
@@ -55,45 +56,55 @@
           "/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt"
           "/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ"
           "/ip4/104.131.131.82/udp/4001/quic/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ"
-
-          ## ipfs-01.owo.systems
-          "/dnsaddr/ipfs-01.owo.systems/p2p/12D3KooWNGmh5EBpPBXGGcFnrMtBW6u9Z61HgyHAobjo2ANhq1kL"
         ];
         Peering = {
           Peers = [
+            {
+              Addrs = [ 
+                "/dns4/ipfs-01.owo.systems/tcp/4001"
+                "/dns6/ipfs-01.owo.systems/tcp/4001"
+              ];
+              ID = "12D3KooWNGmh5EBpPBXGGcFnrMtBW6u9Z61HgyHAobjo2ANhq1kL";
+            }
+            {
+              Addrs = [ ];
+              ID = "12D3KooWDdHkmE24QM4LdUotNJhLRw4vjT3hEDGxCdsB35wb1usb";
+            }
+            {
+              Addrs = [ ];
+              ID = "12D3KooWE3gerBqRL1YAQf8dkokfmbgVSo6DcDtxW3NTsJJRqbux";
+            }
             {
               Addrs = [ ];
               ID = "12D3KooWM63pJ1xhDjqKvH8bEyzowwmfB5tP9UndMP2T2WjDBF7Y";
             }
             {
               Addrs = [ ];
-              ID = "12D3KooWDJCyi3EAVBeisRkrRGtEPjEHNA3CKsmwbWbg5mM9eqvZ";
+              ID = "12D3KooWQvpkLdMxMX2xfBGe44nQu2BiLyiuaLvYSXL15Cfwiowk";
             }
             {
-              Addrs = [
-                "/ip6/2a01:4ff:f0:c73c::1/udp/4001/quic/p2p/12D3KooWJTJoJZ49CgoqYe4JnUfXaqDPYiG5bm1ssN6X4v8n9FF2/p2p-circuit"
-              ];
-              ID = "12D3KooWJo2f5EmnUmZFeWxVDHUKdpZmhQ9pVdJ2eQToxNyF5WNm";
+              Addrs = [ ];
+              ID = "12D3KooWQf7Z78nUizJyhETrKx1NgqbxPTRoQ8NhZAeSjhQWRRnQ";
             }
             {
-              Addrs = [ "/dns6/ipfs1.lxd.bsocat.net/tcp/4001" ];
-              ID = "12D3KooWFkQFKVSgmDfUggx5de5wSbAtfegBnashkP8VN8rESRUX";
+              Addrs = [ "/dns6/dl.lxd.bsocat.net/tcp/4001" ];
+              ID = "12D3KooWQvvJkr8fqUAJWcwe6Tysng3AQyKtSBnTG85rW5vm4B67";
             }
             {
               Addrs = [ "/dns6/ipfs.home.bsocat.net/tcp/4001" ];
               ID = "12D3KooWGHPei7QWiX8vJjHgEkoC4QDWcGKdJf9bE8noP1dAWS21";
             }
             {
-              Addrs = [ "/dns6/ipfs2.lxd.bsocat.net/tcp/4001" ];
-              ID = "12D3KooWLSr7JRSYooakhq58vZowUcCaW4ff31tHaGTrWDDaCL8W";
-            }
-            {
               Addrs = [ "/dns6/gnutoo.home.bsocat.net/tcp/4001" ];
               ID = "12D3KooWNoPhenCQSsdfKJvJ8g2R1bHbw7M7s5arykhqJCVd5F2B";
             }
             {
-              Addrs = [ "/dns6/dl.lxd.bsocat.net/tcp/4001" ];
-              ID = "12D3KooWQvvJkr8fqUAJWcwe6Tysng3AQyKtSBnTG85rW5vm4B67";
+              Addrs = [ "/dns6/ipfs1.lxd.bsocat.net/tcp/4001" ];
+              ID = "12D3KooWFkQFKVSgmDfUggx5de5wSbAtfegBnashkP8VN8rESRUX";
+            }
+            {
+              Addrs = [ "/dns6/ipfs2.lxd.bsocat.net/tcp/4001" ];
+              ID = "12D3KooWLSr7JRSYooakhq58vZowUcCaW4ff31tHaGTrWDDaCL8W";
             }
             {
               Addrs = [ "/dns6/ipfs3.lxd.bsocat.net/tcp/4001" ];
@@ -103,7 +114,7 @@
         };
         Datastore = { StorageMax = "1000GB"; };
         Addresses = {
-          Api = "/ip4/127.0.0.1/tcp/5001";
+          Api = [ "/ip4/127.0.0.1/tcp/5001" "/ip4/10.18.0.1/tcp/5001" ];
           Gateway = "/ip4/127.0.0.1/tcp/8080";
         };
       };
