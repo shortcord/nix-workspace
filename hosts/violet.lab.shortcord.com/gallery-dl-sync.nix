53167cbd8fce9fe2b3117c7ba96e6f3867446a42
Short Cord <short@shortcord.com>
2024-04-14 21:02:11 -0400

    Added new artist

--- hosts/violet.lab.shortcord.com/gallery-dl-sync.nix
+++ hosts/violet.lab.shortcord.com/gallery-dl-sync.nix
@@ -72,6 +72,7 @@ let
     https://danbooru.donmai.us/posts?tags=the_atko
     https://danbooru.donmai.us/posts?tags=aki_(1360505188)
     https://danbooru.donmai.us/posts?tags=hallelujah_zeng
+    https://danbooru.donmai.us/posts?tags=legend_knit
 
     # Characters
     https://danbooru.donmai.us/posts?tags=nazrin
