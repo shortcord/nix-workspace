87f782985efd25a8a5b69609361d8371cdb5dc13
Short Cord <short@shortcord.com>
2024-07-06 12:38:38 -0500

    Updates to postgres on lilac

--- hosts/lilac.lab.shortcord.com/postgresql.nix
+++ hosts/lilac.lab.shortcord.com/postgresql.nix
@@ -1,34 +1,17 @@
-{ pkgs, config, ... }:
-{
-    services = {
-        postgresqlBackup = {
-            enable = true;
-            compression = "zstd";
-            databases = [
-                config.services.mastodon.database.name
-                "matrix_synapse"
-            ];
-        };
-        postgresql = {
-            enable = true;
-            ensureDatabases = [
-                config.services.mastodon.database.name
-                "matrix_synapse"
-            ];
-            ensureUsers = [
-                {
-                    name = config.services.mastodon.database.user;
-                    ensurePermissions = {
-                        "DATABASE ${config.services.mastodon.database.name}" = "ALL PRIVILEGES";
-                    };
-                }
-                {
-                    name = "matrix_synapse";
-                    ensurePermissions = {
-                        "DATABASE matrix_synapse" = "ALL PRIVILEGES";
-                    };
-                }
-            ];
-        };
+{ pkgs, config, ... }: {
+  services = {
+    postgresqlBackup = {
+      enable = true;
+      compression = "zstd";
+      databases = [ config.services.mastodon.database.name ];
     };
+    postgresql = {
+      enable = true;
+      ensureDatabases = [ mastConfig.database.name ];
+      ensureUsers = [{
+        name = mastConfig.database.user;
+        ensureDBOwnership = true;
+      }];
+    };
+  };
 }
