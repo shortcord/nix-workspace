bf483b09b789ea878f49127fb906bc2c155875be
Short Cord <short@shortcord.com>
2024-12-21 10:10:09 -0600

    Merged configs from violet, cause I'm a bad

--- hosts/violet.lab.shortcord.com/nginx.nix
+++ hosts/violet.lab.shortcord.com/nginx.nix
@@ -25,5 +25,25 @@
         };
       };
     };
+    streamConfig = "
+      upstream twitch {
+        hash $remote_addr consistent;
+        server ingest.global-contribute.live-video.net:1935;
+      }
+
+      upstream owncast {
+        server owncast.owo.solutions:1935;
+      }
+
+      server {
+        listen 100.64.0.4:1935 reuseport;
+        proxy_pass twitch;
+      }
+
+      server {
+        listen 100.64.0.4:1936 reuseport;
+        proxy_pass owncast;
+      }
+    ";
   };
 }
