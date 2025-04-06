5c794a2a5ab9eaba3b8f2e58c684b3b06be893ce
Short Cord <short@shortcord.com>
2025-04-06 15:16:34 -0500

    New Machine

---
+++ hosts/lavender.lab.shortcord.com.nix
@@ -0,0 +1,83 @@
+{ name, nodes, pkgs, lib, config, ... }: {
+  system.stateVersion = "23.11";
+
+  imports = [ ./general/dyndns.nix ];
+
+  fileSystems = {
+    "/" = {
+      device = "/dev/disk/by-label/NIXROOT";
+      fsType = "xfs";
+    };
+    "/boot" = {
+      device = "/dev/disk/by-label/NIXBOOT";
+      fsType = "vfat";
+    };
+  };
+
+  systemd = {
+    network = {
+      enable = true;
+      networks = {
+        "10-lan" = {
+          matchConfig.MACAddress = "14:18:77:5b:a9:87";
+          linkConfig = {
+            RequiredForOnline = true;
+          };
+          networkConfig = {
+            DHCP = "yes";
+            DNS = "127.0.0.1";
+          };
+        };
+      };
+    };
+  };
+
+  zramSwap.enable = true;
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+  hardware.enableRedistributableFirmware = true;
+
+  boot = {
+    kernelPackages = pkgs.linuxKernel.packages.linux_6_1;
+    growPartition = true;
+    kernelModules = [ ];
+    extraModulePackages = [ ];
+    kernelParams = [ "kvm-intel" ];
+    loader.systemd-boot = {
+      enable = true;
+      configurationLimit = 5;
+    };
+    initrd = {
+      availableKernelModules = [
+        "ehci_pci"
+        "ahci"
+        "megaraid_sas"
+        "3w_sas"
+        "usbhid"
+        "usb_storage"
+        "sd_mod"
+        "sr_mod"
+      ];
+      kernelModules = [ ];
+    };
+  };
+
+  networking = {
+    useDHCP = false;
+    useNetworkd = true;
+    firewall = {
+      enable = false;
+      allowPing = true;
+    };
+  };
+
+  services = {
+    pdns-recursor = {
+      enable = true;
+      dns = {
+        port = 53;
+        address = [ "127.0.0.1" "::1" ];
+      };
+    };
+  };
+}
