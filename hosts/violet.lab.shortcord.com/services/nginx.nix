c9a6f65efdf14f71cb1ac0f4a572353e84f7ac93
Short Cord <short@shortcord.com>
2025-05-24 13:20:36 -0500

    Formatting

--- hosts/violet.lab.shortcord.com/services/nginx.nix
+++ hosts/violet.lab.shortcord.com/services/nginx.nix
@@ -8,7 +8,23 @@
     recommendedGzipSettings = true;
     recommendedProxySettings = true;
     recommendedBrotliSettings = true;
-    streamConfig =
-      "\n      upstream twitch {\n        hash $remote_addr consistent;\n        server ingest.global-contribute.live-video.net:1935;\n      }\n\n      upstream owncast {\n        server owncast.owo.solutions:1935;\n      }\n\n      server {\n        listen 100.64.0.4:1935 reuseport;\n        proxy_pass twitch;\n      }\n\n      server {\n        listen 100.64.0.4:1936 reuseport;\n        proxy_pass owncast;\n      }\n    ";
+    streamConfig = ''
+      upstream twitch {
+        hash $remote_addr consistent;
+        server ingest.global-contribute.live-video.net:1935;
+      }
+      upstream owncast {
+        server owncast.owo.solutions:1935;
+      }
+      
+      server {
+        listen 100.64.0.4:1935 reuseport;
+        proxy_pass twitch;
+      }
+      server {
+        listen 100.64.0.4:1936 reuseport;
+        proxy_pass owncast;
+      }
+      '';
   };
 }
