117d7e0e76597e46ca8ff4f54231fad3ebe72bc5
Short Cord <short@shortcord.com>
2025-05-25 17:12:40 -0500

    Added gateway to config
    * Cleaned up old hosts

--- config/default.nix
+++ config/default.nix
@@ -2,9 +2,8 @@
   sshkeys = {
     users = {
       short = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINaxLI7oCJcUxfjGXXgs9YI7DimlFbtWE+R22jDF6Zxl short@maus"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICqcmQeDrJdqJFCd25dJzW4YB298X98ls9v24LCjzne2 short@Mauss-MacBook-Pro.local"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPrDWhYX7ALenoIyMYh6MzYBGbbgSLSWbz9EfzEkxo0k short@dell-g3"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINaxLI7oCJcUxfjGXXgs9YI7DimlFbtWE+R22jDF6Zxl short@maus.short.ts.shortcord.com"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPrDWhYX7ALenoIyMYh6MzYBGbbgSLSWbz9EfzEkxo0k short@dell-g3.short.ts.shortcord.com"
       ];
       deployment = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOzNDt0mA8dV9l5A/1tIgLVBf6ynUjjZN0Dckvs3kRIG deployment@gitlab.shortcord.com"
@@ -17,11 +16,10 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP7tbNsAdbSQXOTNpGKKN3mCSIUGn+hhaAqPzA7gh/Hj storage.owo.systems"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOqzy6mgAsJLPsYnYb6sWBvsmZKF8QG7lLE3A/yE55G7 vm01.hetzner.owo.systems"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP4tWdXuPUVt2Yv/cGig8Hilm/NZgpsqH1VbKYpOvnwh lilac.lab.shortcord.com"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMYLWI4ixfYTbbGymECI2zKPsXPzjoJYDPcb/8R4ptX+ matrix.mousetail.dev"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJEfHEMvtBanjtNLBpzyZwDNA5ro7ORIFARnKeIj/GgS miauws.life"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHw2ZJN9Mk/MILgpdDQ3a3/o7SCPHvbT/iaWWMnMWM3O keycloak.owo.solutions"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGQk+htjVN1ywCnUXY3LeiyNOV6bbp3KKXbSCKQx7mHV hydra.owo.solutions"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAJJkiqaqs67EEzd73X9RQu6vvya64EgtDQBGf4vQ5LN lavender.lab.shortcord.com"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICcXYPR1tmuJVoFlW1Ngq7Ixq8/R2SLZXUlcs3We/cKm gateway.home.shortcord.com"
       ];
     };
   };
