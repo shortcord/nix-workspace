95a2b2a9d22d529ab1f6aa57263775111406d44c
Short Cord <short@shortcord.com>
2025-06-02 21:24:19 -0500

    use correct prefix
    idk when i did this but I did

--- build-and-upload-to-pve.sh
+++ build-and-upload-to-pve.sh
@@ -6,6 +6,6 @@ if [[ -z "${1}" ]]; then
     exit 1
 fi
 
-nix build ".#vm-${1}" || ( echo "nix broke :("; exit 1; )
+nix build ".#vm.vm-${1}" || ( echo "nix broke :("; exit 1; )
 
 rsync -P --copy-links result/vzdump-*.vma.zst root@pve.owo.solutions:"/var/lib/vz/dump/vzdump-nixos-${1}.vma.zst" || ( echo "rsync broke :(("; exit 1; )
