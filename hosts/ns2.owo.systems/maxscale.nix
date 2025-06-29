a896e5680e692696c5284c51f4b875865677b044
Short Cord <short@shortcord.com>
2025-06-28 22:28:45 -0500

    I must have done this while high.

--- hosts/ns2.owo.systems/maxscale.nix
+++ hosts/ns2.owo.systems/maxscale.nix
@@ -4,7 +4,7 @@
       autoStart = true;
       image = "docker.io/mariadb/maxscale:latest";
       volumes = [ "maxscale-config:/var/lib/maxscale/:rw" ];
-      ports = [ "3366:3366" "127.0.0.1:8989" ];
+      ports = [ "3366:3366" ];
     };
   };
 }
