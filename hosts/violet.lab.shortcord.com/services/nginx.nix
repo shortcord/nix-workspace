a8859d280e2207d33ad83af0493f31597815a002
Short Cord <short@shortcord.com>
2025-10-26 00:23:25 -0500

    just work

--- hosts/violet.lab.shortcord.com/services/nginx.nix
+++ hosts/violet.lab.shortcord.com/services/nginx.nix
@@ -8,23 +8,5 @@
     recommendedGzipSettings = true;
     recommendedProxySettings = true;
     recommendedBrotliSettings = true;
-    streamConfig = ''
-      upstream twitch {
-        hash $remote_addr consistent;
-        server ingest.global-contribute.live-video.net:1935;
-      }
-      upstream owncast {
-        server owncast.owo.solutions:1935;
-      }
-      
-      server {
-        listen 100.64.0.4:1935 reuseport;
-        proxy_pass twitch;
-      }
-      server {
-        listen 100.64.0.4:1936 reuseport;
-        proxy_pass owncast;
-      }
-      '';
   };
 }
