c10d062b264b592a63c1f0b26271c6dc3607dd1a
Short Cord <short@shortcord.com>
2026-01-21 23:28:47 -0600

    Updated container base

--- containers/general/container-base.nix
+++ containers/general/container-base.nix
@@ -10,6 +10,9 @@
         manageHostName = false;
     };
 
+    # This key exists on the proxmox hypervisor, a deployment
+    # script will bind-mount it into the container so that
+    # the container can decrypt general secrets on first boot. 
     age.identityPaths = lib.mkAfter [
         "/agenix/shared-container-key"
     ];
